7b6f712ef5f7d6c80e9878060e1fbd8b9eeb2be7
linjj <linjunjie2569@foxmail.com>
2025-09-21 02:56:24 +0800

    [update] update postgresql adpater in generator

--- ruoyi-admin/src/main/java/org/dromara/DromaraApplication.java
+++ ruoyi-admin/src/main/java/org/dromara/DromaraApplication.java
@@ -1,6 +1,7 @@
 package org.dromara;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
@@ -17,7 +18,7 @@ public class DromaraApplication {
         SpringApplication application = new SpringApplication(DromaraApplication.class);
         application.setApplicationStartup(new BufferingApplicationStartup(2048));
         application.run(args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  RuoYi-Vue-Plus启动成功   ლ(´ڡ`ლ)ﾞ");
+        System.out.println("启动成功");
     }
 
 }
